fb93cf00c05bdc1431f3d0c60e856d3fd95d3d46
Cole Glotfelty <16599506+cole-glotfelty@users.noreply.github.com>
2023-04-14 18:11:28 -0400

    encoder test

--- test/motor_test.py
+++ test/motor_test.py
@@ -1,9 +1,11 @@
-from gpiozero import PhaseEnableMotor
+from gpiozero import PhaseEnableMotor, RotaryEncoder
 from time import sleep
 
 motor1 = PhaseEnableMotor(19, 26)
 motor2 = PhaseEnableMotor(6, 13)
+rotar1 = RotaryEncoder(16, 18, max_step=12)
 motor1.forward()
 motor2.forward()
+print(rotar1.value())
 
 sleep(3)
