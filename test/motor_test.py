5db4b45a59bf14c0bdb628449f87e9d6805b15d3
Cole Glotfelty <16599506+cole-glotfelty@users.noreply.github.com>
2023-04-14 18:49:38 -0400

    pls work

--- test/motor_test.py
+++ test/motor_test.py
@@ -3,7 +3,7 @@ from time import sleep
 
 motor1 = PhaseEnableMotor(19, 26)
 motor2 = PhaseEnableMotor(6, 13)
-rotar1 = RotaryEncoder(16, 18, max_steps=12)
+rotar1 = RotaryEncoder(23, 24, max_steps=12)
 motor1.forward()
 motor2.forward()
 while True:
