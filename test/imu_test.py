cbcdf85ec1778c543fa2927f14fca6742e94317f
EmmaB3 <emma.l.bethel@gmail.com>
2023-04-19 18:31:20 -0400

    tests for all sensors

---
+++ test/imu_test.py
@@ -0,0 +1,48 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+import adafruit_bno055
+import adafruit_tca9548a
+
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+
+# multiplexer
+tca = adafruit_tca9548a.TCA9548A(i2c)
+
+# imu (attached to port 0 of multiplexer)
+sensor = adafruit_bno055.BNO055_I2C(tca[0])
+
+last_val = 0xFFFF
+
+
+def temperature():
+    global last_val  # pylint: disable=global-statement
+    result = sensor.temperature
+    if abs(result - last_val) == 128:
+        result = sensor.temperature
+        if abs(result - last_val) == 128:
+            return 0b00111111 & result
+    last_val = result
+    return result
+
+
+while True:
+    print("Temperature: {} degrees C".format(sensor.temperature))
+    """
+    print(
+        "Temperature: {} degrees C".format(temperature())
+    )  # Uncomment if using a Raspberry Pi
+    """
+    print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
+    print("Magnetometer (microteslas): {}".format(sensor.magnetic))
+    print("Gyroscope (rad/sec): {}".format(sensor.gyro))
+    print("Euler angle: {}".format(sensor.euler))
+    print("Quaternion: {}".format(sensor.quaternion))
+    print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
+    print("Gravity (m/s^2): {}".format(sensor.gravity))
+    print()
+
+    time.sleep(1)
--- test/motor_test.py
+++ test/motor_test.py
@@ -3,11 +3,13 @@ from time import sleep
 
 motor1 = PhaseEnableMotor(19, 26)
 motor2 = PhaseEnableMotor(6, 13)
-rotar1 = RotaryEncoder(23, 24, max_steps=12)
-motor1.forward()
-motor2.forward()
+rotar1 = RotaryEncoder(23, 24, max_steps=0)
+rotar2 = RotaryEncoder(14, 15, max_steps=0)
+motor1.forward(0.5)
+motor2.forward(0.5)
+motor1.stop()
+motor2.stop()
 while True:
-    print(rotar1.value)
+    print(rotar1.steps, rotar2.steps)
+    sleep(0.1)
 
-
-sleep(3)
